7240f14e1a1e3093e4afe68e853b339c4fb508e5
brandon <lim.wx.brandon@gmail.com>
2023-10-28 14:00:37 +0800

    Used class syntax instead

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function info() {
